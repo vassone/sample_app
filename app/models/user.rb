aafcd79a084e48c5574d7dca6bc7c7c7dd2a8636
Dariusz Kwaśny <d.kwasny@post.pl>
2010-05-21 18:20:58 +0200

    Added password validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,10 +14,18 @@ class User < ActiveRecord::Base
 
   EmailRegex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
-  attr_accessible :name, :email
+  attr_accessor :password # virtual attribute, in memory only, not in db
+  attr_accessible :name, :email, :password, :password_confirmation
+
   validates_presence_of :name, :email
   validates_length_of :name, :maximum => 50
   validates_format_of :email, :with => EmailRegex
   validates_uniqueness_of :email, :case_sensitive => false
+  
+  # Automatically create the virtual attribute "password_confirmation".
+  validates_confirmation_of :password
 
+  # Password validations.
+  validates_presence_of :password
+  validates_length_of :password, :within => 6..40  
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,7 +16,9 @@ describe User do
   before(:each) do
     @attr = {
       :name => "Example User",
-      :email => "user@example.com"
+      :email => "user@example.com",
+      :password => "foobar",
+      :password_confirmation => "foobar"
     }
   end
 
@@ -61,6 +63,33 @@ describe User do
     User.create!(@attr.merge(:email => upcased_email))
     user_with_duplicate_email = User.new(@attr)
     user_with_duplicate_email.should_not be_valid
+  end
+
+  describe "password validations" do
+
+    it "should require a password" do
+      User.new(@attr.merge(:password => "", :password_confirmation => "")).
+        should_not be_valid
+      
+    end 
+
+    it "should require matching password confirmation" do
+      User.new(@attr.merge(:password_confirmation => "invalid")).
+        should_not be_valid
+    end
+
+    it "should reject short passwords" do
+      short = "a" * 5
+      hash = @attr.merge(:password => short, :password_confirmation => short) 
+      User.new(hash).should_not be_valid
+    end
+
+    it "should reject too long passwords" do
+      long = "a" * 41
+      hash = @attr.merge(:password => long, :password_confirmation => long)
+      User.new(hash).should_not be_valid
+    end
+
   end
 
 end
